4917d67b40e686f959c49f42072429e32c1da41a
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-05-01 17:53:42 +0300

    .

--- Action/Program.cs
+++ Action/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Action
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
+            string message = "test";
+            Action<string> print;
+            print = Print;
+            print(message);
+
+            Func<int, int, int> sum;
+            sum = Sum;
+
+            Func<int[], double> max;
+            max = Max;
+
+            Action<byte[]> send;
+            send = SendData;
+
+            Some some;
+            some = SomeFunc;
+
+        }
+        delegate void Some();
+
+        private static void Print(string message)
+        {
+
+        }
+
+        private static int Sum(int a, int b)
+        {
+            return a;
+        }
+
+        private static double Max(int[] array)
+        {
+            return 5.0;
+        }
+
+        private static void SendData(byte[] data)
+        {
+
+        }
+
+        private static void SomeFunc()
+        {
+
         }
     }
 }
